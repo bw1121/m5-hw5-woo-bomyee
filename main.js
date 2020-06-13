fbdf7a2ee7d85cb95b8746c109f89342fd9bdad8
Bomyee Woo <bomyeew@gmail.com>
2020-06-13 09:54:03 -0400

    Add click function to box 2 & 3

--- main.js
+++ main.js
@@ -14,10 +14,11 @@ for (i = 0; i < numberBox.length; i++) {
 
 //Box click and message appears
 //create h3 element
-var addText = document.createElement("h3");
+var addText = document.createElement('h3');
 //texts
 var text1 = document.createTextNode('Oooh - so close, but no cigar');
 var text2 = document.createTextNode('DING DING DING - We have a winner')
+var text3 = document.createTextNode('Oops, better luck next time')
 
 
 //box 1 click
@@ -26,4 +27,18 @@ function box1Message() {
     document.querySelector('.text').appendChild(addText)
 };
 
+//box 2 click
+function box2Message() {
+    addText.appendChild(text2);
+    document.querySelector('.text').appendChild(addText)
+};
+
+//box 3 click
+function box3Message() {
+    addText.appendChild(text3);
+    document.querySelector('.text').appendChild(addText)
+};
+
 numberBox[0].addEventListener('click', box1Message);
+numberBox[1].addEventListener('click', box2Message);
+numberBox[2].addEventListener('click', box3Message);
