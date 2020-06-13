3af876db65d8b8825d0ccb3ca0d53158eb96aeab
Bomyee Woo <bomyeew@gmail.com>
2020-06-13 09:22:35 -0400

    Fix selector for box 1 click

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ for (i = 0; i < numberBox.length; i++) {
 }
 
 //Box click and message appears
-var boxes = document.getElementsByClassName('boxes');
 //create h3 element
 var addText = document.createElement("h3");
 //texts
 var text1 = document.createTextNode('Oooh - so close, but no cigar');
+var text2 = document.createTextNode('DING DING DING - We have a winner')
 
 
-//box1 click
+//box 1 click
 function box1Message() {
     addText.appendChild(text1);
     document.querySelector('.text').appendChild(addText)
 };
 
-boxes[0].addEventListener('click', box1Message);
+numberBox[0].addEventListener('click', box1Message);
