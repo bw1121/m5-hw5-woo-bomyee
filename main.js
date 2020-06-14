f6cabc05561eaf267b90903bb0348b903573ebb7
Bomyee Woo <bomyeew@gmail.com>
2020-06-14 09:53:04 -0400

    Fix & organize Javascript

--- main.js
+++ main.js
@@ -13,30 +13,31 @@ for (i = 0; i < numberBox.length; i++) {
 }
 
 //Box click and message appears
-//create h3 element
+//create element
 var addText = document.createElement('h3');
+
 //texts
 var text1 = document.createTextNode('Oooh - so close, but no cigar');
-var text2 = document.createTextNode('DING DING DING - We have a winner')
-var text3 = document.createTextNode('Oops, better luck next time')
+var text2 = document.createTextNode('DING DING DING - We have a winner');
+var text3 = document.createTextNode('Oops, better luck next time');
 
 
 //box 1 click
 function box1Message() {
     addText.appendChild(text1);
-    document.querySelector('.text').appendChild(addText)
+    document.querySelector('.text').appendChild(addText);
 };
 
 //box 2 click
 function box2Message() {
     addText.appendChild(text2);
-    document.querySelector('.text').appendChild(addText)
+    document.querySelector('.text').appendChild(addText);
 };
 
 //box 3 click
 function box3Message() {
     addText.appendChild(text3);
-    document.querySelector('.text').appendChild(addText)
+    document.querySelector('.text').appendChild(addText);
 };
 
 numberBox[0].addEventListener('click', box1Message);
